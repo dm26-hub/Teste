f38f2ce37dcfdece9d24b9d94ab048dac9211a93
dm26-hub <david.ribeiro@ba.estudante.senai.br>
2026-03-23 21:25:14 -0300

    Add files via upload

---
+++ Vetor.js
@@ -0,0 +1,13 @@
+let vetor = [9,99,1,3,5,7]
+
+
+if (vetor [0] > 10) {console.log ("Número maior que 10")}
+else (console.log ("Não é maior que 10"))
+
+
+
+
+let notas = [4,8,9]
+if (notas [0] >= 7 && notas [1] >= 7 && notas [2] >= 7) {console.log ("Aprovado Direto")}
+else if (notas [0]<= 4 || notas [1]<= 4 || notas [2]<= 4) {console.log ("Reprovado")}
+else (console.log ("Recuperação"))
