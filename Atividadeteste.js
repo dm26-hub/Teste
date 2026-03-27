cb2241e63a8902ff3bd0038897f3747d7565161b
dm26-hub <david.ribeiro@ba.estudante.senai.br>
2026-03-26 21:32:48 -0300

    Add files via upload

---
+++ Atividadeteste.js
@@ -0,0 +1,14 @@
+let empresa = [150,95,175,200,30,45,99]
+let diaria = 0;
+let acimade100 = 0;
+let soma = 0
+let dias = Math.max (empresa,[...acimade100])
+for (i = 0; i < empresa.length; i++)
+{console.log (empresa[i])  
+diaria += empresa[i]/7;
+soma += empresa [i]
+}
+
+console.log ("A soma total é " + soma)
+    console.log ("A diária média é " + diaria)
+    console.log ("Total de dias maior que 100 = " + dias)
